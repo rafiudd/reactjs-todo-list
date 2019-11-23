dc77d0453f07f95384c9b6cced382c614e6aa728
rafiudd <sirin6867@gmail.com>
2019-11-23 14:14:37 +0700

    Updates

---
+++ precache-manifest.163594c3645e1a32a61b0db082bdc2c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139cc004176f8c6327b4c8d9ef2dc90b",
+    "url": "/reactjs-todo-list/index.html"
+  },
+  {
+    "revision": "cb315f20e31a98384c98",
+    "url": "/reactjs-todo-list/static/css/main.3b8d9581.chunk.css"
+  },
+  {
+    "revision": "237deea7b7dd15dce20c",
+    "url": "/reactjs-todo-list/static/js/2.34265c10.chunk.js"
+  },
+  {
+    "revision": "cb315f20e31a98384c98",
+    "url": "/reactjs-todo-list/static/js/main.1a39db81.chunk.js"
+  },
+  {
+    "revision": "dd2c1df852ba1d4494dd",
+    "url": "/reactjs-todo-list/static/js/runtime-main.80d38c06.js"
+  }
+]);
